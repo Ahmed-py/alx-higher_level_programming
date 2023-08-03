dd58c6fc44c77a7759f8276128290454b5ddc7a8
Ahmed-py <eng_ahmed_hamed2016@yahoo.com>
2023-08-03 12:54:41 -0700

    0x05-python-exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    result = 0
     try:
-        result = a / b
-    except:
-        result = None
+        if b != 0:
+            result = a / b
+            return result
+        else:
+            result = None
+    except ValueError:
+        pass
     finally:
         print("Inside result: {}".format(result))
-        return (result)
